f398956dc1d4994d722085152fc2b0b9ae590309
zxcv5500 <zxcv5500@gmail.com>
2018-10-30 00:55:55 +0900

    realm 인트로 액티비티 생성

---
+++ app/src/main/java/com/zxcv5500/toyrealmproject/intro/IntroExampleActivity.java
@@ -0,0 +1,15 @@
+package com.zxcv5500.toyrealmproject.intro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zxcv5500.toyrealmproject.R;
+
+public class IntroExampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realm_basic_example);
+    }
+}
