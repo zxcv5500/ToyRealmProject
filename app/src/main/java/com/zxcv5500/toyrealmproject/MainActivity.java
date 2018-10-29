9fa6d258b42610165568ff481ecdbe910b2fb7e4
zxcv5500 <zxcv5500@gmail.com>
2018-10-29 22:57:50 +0900

    Initial commit

---
+++ app/src/main/java/com/zxcv5500/toyrealmproject/MainActivity.java
@@ -0,0 +1,239 @@
+package com.zxcv5500.toyrealmproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    // 예제가 있는 시작 챕터. 스피너의 첨자에 이 값을 더해야 장 번호가 된다.
+    static final int START_CHAPTER = 1;
+
+    class Example {
+        Class<?> cls;
+        String name;
+        String desc;
+
+        Example(Class<?> acls, String aDesc) {
+            cls = acls;
+            name = acls.getSimpleName();
+            desc = aDesc;
+        }
+    }
+
+    ArrayList<Example> arExample = new ArrayList<Example>();
+
+    // 요청한 장의 예제들을 배열에 채운다.
+    void fillExample(int chapter) {
+        arExample.clear();
+
+        switch (chapter) {
+            case 1:
+                //arExample.add(new Example(TextViewTest.class, "텍스트 뷰 위젯 소개. 3개의 문자열 출력"));
+                break;
+        }
+    }
+
+    String[] arChapter = {
+            "1장 인트로",
+    };
+
+    ArrayAdapter<CharSequence> mAdapter;
+    ListView mExamList;
+    Spinner mSpinner;
+    boolean mInitSelection = true;
+    int mFontSize;
+    int mBackType;
+    boolean mDescSide;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mExamList = (ListView)findViewById(R.id.examlist);
+        mSpinner = (Spinner)findViewById(R.id.spinnerchapter);
+        mSpinner.setPrompt("장을 선택하세요.");
+
+        mAdapter = new ArrayAdapter<CharSequence>(this,
+                android.R.layout.simple_spinner_item, arChapter);
+        mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(mAdapter);
+
+        mSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 최초 전개시에도 Selected가 호출되는데 이때는 프레퍼런스에서 최후 장을 찾아 로드한다.
+                // 이후부터는 사용자가 선택한 장을 로드한다.
+                if (mInitSelection) {
+                    mInitSelection = false;
+                    SharedPreferences pref = getSharedPreferences("AndExam", 0);
+                    int lastchapter = pref.getInt("LastChapter", START_CHAPTER);
+                    mSpinner.setSelection(lastchapter - START_CHAPTER);
+                    changeChapter(lastchapter);
+                } else {
+                    // 장을 변경할 때마다 프레퍼런스에 기록한다.
+                    int chapter = position + START_CHAPTER;
+                    changeChapter(chapter);
+                    SharedPreferences pref = getSharedPreferences("AndExam", 0);
+                    SharedPreferences.Editor edit = pref.edit();
+                    edit.putInt("LastChapter", chapter);
+                    edit.commit();
+                }
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        readOption();
+
+        // 자동실행 옵션의 디폴트는 false로 설정한다. 한 예제만 반복적으로 테스트할 때 이 옵션을
+        // 사용하되 예외 처리가 어려우므로 왠만하면 사용하지 않는 것이 좋다.
+        boolean bRunLast = false;
+        if (bRunLast) {
+            SharedPreferences pref = getSharedPreferences("ToyRealmExam", 0);
+            int pkg = pref.getInt("LastChapter", START_CHAPTER);
+            int pos = pref.getInt("LastPosition", 0);
+            changeChapter(pkg);
+            Intent intent = new Intent(this, arExample.get(pos).cls);
+            startActivity(intent);
+        }
+    }
+
+    public void readOption() {
+        SharedPreferences pref = getSharedPreferences("ToyRealmExam", 0);
+        mFontSize = pref.getInt("mFontSize", 1);
+        mBackType = pref.getInt("mBackType", 0);
+        mDescSide = pref.getBoolean("mDescSide", false);
+
+        if (mBackType != 0) {
+            mExamList.setBackgroundColor(0xff101010);
+            mExamList.setDivider(new ColorDrawable(0xff606060));
+            mExamList.setDividerHeight(1);
+        } else {
+            mExamList.setBackgroundColor(0xffe0e0e0);
+            mExamList.setDivider(new ColorDrawable(0xff404040));
+            mExamList.setDividerHeight(1);
+        }
+    }
+
+    // 장을 변경한다. 인수로 전달되는 chapter는 첨자가 아니라 장 번호이다.
+    public void changeChapter(int chapter) {
+        fillExample(chapter);
+        ExamListAdapter adapter = new ExamListAdapter(this);
+        mExamList.setAdapter(adapter);
+
+        final Context ctx = this;
+        mExamList.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences pref = getSharedPreferences("ToyRealmExam", 0);
+                SharedPreferences.Editor edit = pref.edit();
+                edit.putInt("LastPosition", position);
+                edit.commit();
+                Intent intent = new Intent(ctx, arExample.get(position).cls);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void mOnClick(View v) {
+        SharedPreferences pref = getSharedPreferences("ToyRealmExam", 0);
+        int lastchapter = pref.getInt("LastChapter", START_CHAPTER);
+        switch (v.getId()) {
+            case R.id.btnprev:
+                if (lastchapter !=  START_CHAPTER) {
+                    lastchapter--;
+                    mSpinner.setSelection(lastchapter - START_CHAPTER);
+                }
+                break;
+            case R.id.btnnext:
+                if (lastchapter != arChapter.length - 1 + START_CHAPTER) {
+                    lastchapter++;
+                    mSpinner.setSelection(lastchapter - START_CHAPTER);
+                }
+                break;
+        }
+    }
+
+    //어댑터 클래스
+    class ExamListAdapter extends BaseAdapter {
+        Context maincon;
+        LayoutInflater Inflater;
+
+        public ExamListAdapter(Context context) {
+            maincon = context;
+            Inflater = (LayoutInflater)context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public int getCount() {
+            return arExample.size();
+        }
+
+        public String getItem(int position) {
+            return arExample.get(position).name;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        // 각 항목의 뷰 생성
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = Inflater.inflate(R.layout.andexamlist, parent, false);
+            }
+
+            LinearLayout examlayout = (LinearLayout)convertView.findViewById(R.id.examlayout);
+            TextView txt1 = (TextView)convertView.findViewById(R.id.text1);
+            TextView txt2 = (TextView)convertView.findViewById(R.id.text2);
+
+            if (mDescSide) {
+                examlayout.setOrientation(LinearLayout.HORIZONTAL);
+            }
+
+            if (mBackType != 0) {
+                examlayout.setBackgroundResource(R.drawable.exambackdark);
+                txt1.setTextColor(Color.WHITE);
+                txt2.setTextColor(Color.LTGRAY);
+            }
+
+            switch (mFontSize) {
+                case 0:
+                    txt1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13);
+                    txt2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 9);
+                    break;
+                case 1:
+                    txt1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+                    txt2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11);
+                    break;
+                case 2:
+                    txt1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+                    txt2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+                    break;
+            }
+
+            txt1.setText(arExample.get(position).name);
+            txt2.setText(arExample.get(position).desc);
+
+            return convertView;
+        }
+    }
+}
